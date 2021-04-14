de4e219cf6daf1773422e479623c09fadaf3cc6b
ronaldemanuel <emronald15el@gmail.com>
2021-04-13 23:10:59 -0300

    Criando construtor, destrutor e usando atributos e metodos estaticos

--- Orientação a Objetos 1/banco.php
+++ Orientação a Objetos 1/banco.php
@@ -2,8 +2,24 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
-$primeiraConta->sacar(300);
-$primeiraConta->saldo -= 300;
+$primeiraConta = new Conta('123.456.789-10', 'Ronald Emanuel');
+var_dump($primeiraConta);
 
-echo $primeiraConta->saldo;
+$primeiraConta->depositar(500);
+$primeiraConta->sacar(300);
+
+echo $primeiraConta->getNomeTitular() . PHP_EOL;
+echo $primeiraConta->getCpfTitular() . PHP_EOL;
+echo $primeiraConta->getSaldo() . PHP_EOL;
+
+$segundaConta = new Conta('987.654.321-10', 'Maria Cecília');
+
+$primeiraConta->transferir(50, $segundaConta);
+
+echo $segundaConta->getNomeTitular() . PHP_EOL;
+echo $segundaConta->getCpfTitular() . PHP_EOL;
+echo $segundaConta->getSaldo() . PHP_EOL;
+
+$conta3 = new Conta('634.928.736-91', 'Jose da Silva');
+
+echo Conta::getNumeroDeContas() . PHP_EOL;
--- Orientação a Objetos 1/src/Conta.php
+++ Orientação a Objetos 1/src/Conta.php
@@ -5,8 +5,24 @@ class Conta
     private string $cpfTitular;
     private string $nomeTitular;
     private float $saldo = 0;
+    private static int $numeroDeContas = 0;
 
-    public function sacar(float $valor) : void
+    public function __construct(string $cpfTitular, string $nomeTitular)
+    {
+        $this->cpfTitular = $cpfTitular;
+        $this->validarNomeTitular($nomeTitular);
+        $this->nomeTitular = $nomeTitular;
+        $this->saldo = 0;
+
+        self::$numeroDeContas++;
+    }
+
+    public function __destruct()
+    {
+        self::$numeroDeContas--;
+    }
+
+    public function sacar(float $valor): void
     {
         if ($valor > $this->saldo) {
             echo "Saldo indisponível";
@@ -15,7 +31,7 @@ class Conta
         $this->saldo -= $valor;
     }
 
-    public function depositar(float $valor) : void
+    public function depositar(float $valor): void
     {
         if ($valor < 0)  {
             echo "Deposite um valor válido";
@@ -25,7 +41,7 @@ class Conta
         $this->saldo += $valor;
     }
 
-    public function transferir(float $valor, Conta $contaDestino) : void
+    public function transferir(float $valor, Conta $contaDestino): void
     {
         if ($valor > $this->saldo) {
             echo "Saldo indisponível!";
@@ -34,5 +50,34 @@ class Conta
 
         $this->sacar($valor);
         $contaDestino->depositar($valor);   
+    }
+
+    // Métodos getters e setters
+    public function getCpfTitular(): string 
+    {
+        return $this->cpfTitular;
+    }
+
+    public function getNomeTitular(): string
+    {
+        return $this->nomeTitular;
+    }
+
+    public function getSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    private function validarNomeTitular($nomeTitular): void
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "O nome de titular precisa de pelo menos 5 caracteres";
+            exit();
+        }
+    }
+
+    public static function getNumeroDeContas(): int 
+    {
+        return Conta::$numeroDeContas;
     }
 }
