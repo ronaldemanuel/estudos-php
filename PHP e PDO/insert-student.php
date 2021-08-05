9d02eb29e73b49f0a5e3d8178974a414e1d33de3
ronaldemanuel <emronald15el@gmail.com>
2021-08-05 15:46:10 -0300

    Adicionando prepared statements

--- PHP e PDO/insert-student.php
+++ PHP e PDO/insert-student.php
@@ -7,9 +7,17 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/database.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Crispim da Silva', new \DateTimeImmutable('2000-06-15'));
+$student = new Student(
+    null,
+    "Maria José",
+    new \DateTimeImmutable('2002-10-27')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->getName()}', '{$student->getBirthDate()
-    ->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
+$stmt = $pdo->prepare($sqlInsert);
+$stmt->bindValue(':name', $student->getName());
+$stmt->bindValue(':birth_date', $student->getBirthDate()->format('Y-m-d'));
+if ($stmt->execute()) {
+    echo "Aluno adicionado";
+}
 
-var_dump($pdo->exec($sqlInsert));
